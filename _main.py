9f45876e7992049e7d1d6fe7463668d56b5215f5
Alex <alexnb.bg@gmail.com>
2022-01-25 19:21:41 +0200

    added comments

--- _main.py
+++ _main.py
@@ -1,44 +1,63 @@
-# pylint: disable=invalid-name
+# pylint: disable=missing-module-docstring, invalid-name
+
 import utilities
 from single_cell import SingleCell
 from grid import Grid
 from show_grid import ShowGrid
 
 
-# for example
+# Game parameters
 columns = 12    # max 26
 rows = 6
 # density of mines  dens = mines/all cells  (higher value -> less mines)
-dens = 20
+density = 7
 
-# generating a random board
-a_main = utilities.generate_random_string(columns, rows, dens)
+# 1
+# generating a string with random mines
+a_main = utilities.generate_random_string(columns, rows, density)
 
-# Grid
-current_grid = Grid(rows, a_main)
+# 2
+# create a Grid object with the generated string
+current_grid = Grid(columns, a_main)
 
+# 3
 # player input
+# loop until the game is over or an error occur
 while True:
+    # print the game
     ShowGrid.print_board(current_grid)
-    inp: str = input(
+
+    # input from the player
+    input_str: str = input(
         'Enter o/f (for open/flag a cell), column letter and row number (oA3/fA3): '
     ).lower().replace(' ', '')
 
-    inp_column = ord(
-        ''.join([x for x in inp[1:] if not x.isdigit()]).upper()
+    # take index of column and row from the input and create a SingleCell object
+    input_column = ord(
+        ''.join([x for x in input_str[1:] if not x.isdigit()]).upper()
     ) - 65
-    inp_row = int(''.join([x for x in inp[1:] if x.isdigit()])) - 1
+    input_row = int(''.join([x for x in input_str[1:] if x.isdigit()])) - 1
+    # create a SingleCell object with the location of the choosed cell
+    input_cell = SingleCell(input_column, input_row)
 
-    if inp[0] == 'o':
-        current_move = current_grid.open_cell(SingleCell(inp_column, inp_row))
-        ShowGrid.print_board(current_grid)
+    # check and execute the action from the input
+    # open or flag
+    if input_str[0] == 'o':
+        # get response from the Grid object when opening the cell
+        open_cell_response = current_grid.open_cell(input_cell)
+
+        # check if all cells without mines are already opened - game won
         if current_grid.check_game_won:
             ShowGrid.print_board(current_grid, True)
             print('Congratulations! You won the game!\n')
             break
-        if current_move == '9':
+
+        # if the cell has a mine - game lost
+        if open_cell_response == '9':
             ShowGrid.print_board(current_grid, True)
-            print('Game over!\n')
+            print('Game over!')
+            print('You lost!')
+            print()
             break
-    elif inp[0] == 'f':
-        current_grid.flag_cell(SingleCell(inp_column, inp_row))
+    elif input_str[0] == 'f':
+        current_grid.flag_cell(input_cell)
