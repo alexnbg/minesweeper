5f071908a6d723b99214ac49314a08ae8a85fed0
Alex <alexnb.bg@gmail.com>
2022-01-28 13:54:03 +0200

    added conversions for str/list of True/False

--- utilities.py
+++ utilities.py
@@ -2,10 +2,10 @@ import random
 
 
 def generate_random_string(columns: int, rows: int, density: int):
-    """Generates and returns a string of 'T' and 'F'\n
-    with random placed mines and lenght = columns*rows.\n
-    columns/rows - the size of the grid and the string\n
-    density - the number of empty cells per single mine (higher value -> less mines)"""
+    """Generates and returns a string of 'T' and 'F'
+    with random placed mines and lenght = columns*rows.
+      columns/rows (int) : the size of the grid and the string
+      density (int) : the number of empty cells per single mine (higher value -> less mines)"""
 
     mines = int(columns*rows/density) + (columns*rows % density > 0)
     out_list = ['F']*columns*rows
@@ -15,4 +15,26 @@ def generate_random_string(columns: int, rows: int, density: int):
         if out_list[ind] == 'F':
             out_list[ind] = 'T'
 
+    return ''.join(out_list)
+
+
+def conv_str_to_list(tf_string: str) -> list:
+    """Converts a string of 'T' and 'F' into a list of True and False."""
+    out_list = []
+    for cell in tf_string:
+        if cell == 'T':
+            out_list.append(True)
+        elif cell == 'F':
+            out_list.append(False)
+    return out_list
+
+
+def conv_list_to_str(tf_list: list) -> str:
+    """Converts a list of True and False into a string of 'T' and 'F'."""
+    out_list = []
+    for cell in tf_list:
+        if cell:
+            out_list.append('T')
+        else:
+            out_list.append('F')
     return ''.join(out_list)
